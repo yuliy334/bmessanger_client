e5b0b126f517df73052df6d3525f37ce2f390f2a
Yuliy Sherman <yuliy.sherman@gmail.com>
2025-10-22 21:10:01 +0300

    feat: scroll for last messasge

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { Message } from "../Message/Message";
 import { MessangerContainer } from "../MessangerContainer/MessangerContainer";
 import "./ChatStyle.css";
@@ -14,24 +14,32 @@ interface ChatProps {
 
 export function Chat({ ChatId }: ChatProps) {
     const [messsageText, setMessageText] = useState<string>("");
+    const boxRef = useRef<HTMLDivElement | null>(null);
     const chatsContext = useContext(ChatsContext);
     if (!chatsContext) {
         throw new Error("NoChatContext");
     }
     const { state, dispatch } = chatsContext;
     const ChatMessagesId = state.chats.findIndex((a) => a.id == ChatId);
-    console.log(ChatMessagesId);
     async function SendHandel() {
         const messageBody: messageSend = { text: messsageText, chatId: ChatId };
         const NewMessage = await SendMesssage(messageBody);
-        console.log("newMessage:   ",NewMessage);
-        dispatch({type: "set_message", payload:NewMessage});
+        dispatch({ type: "set_message", payload: NewMessage });
         console.log(state);
-        // console.log("dsfsfs", NewMessage);
     }
+
+    useEffect(() => {
+        if (boxRef.current) {
+            console.log("dsfsff");
+            boxRef.current.scrollTop = boxRef.current.scrollHeight;
+        }
+
+    }, [state])
+
+
     return (
         <div className="ChatContainer">
-            <div className="Messages">
+            <div className="Messages"  ref={boxRef}>
                 {state.chats[ChatMessagesId].messages.map((m: message) => (<Message key={m.createdAt.toString() + m.senderName} messageData={m} />))}
 
 
